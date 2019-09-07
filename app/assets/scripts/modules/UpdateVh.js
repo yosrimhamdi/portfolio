d704cd30b1d38fc2fb2a1c2fa82b611f06ad4010
TheeWebMaster <bavary1515@gmail.com>
2019-09-07 18:04:36 +0100

    fixed vh issue on mobile devices.

---
+++ app/assets/scripts/modules/UpdateVh.js
@@ -0,0 +1,18 @@
+class UpdateVh {
+  constructor() {
+    this.root = document.documentElement;
+    this.updateVh();
+    this.events();
+  }
+  events() {
+    window.addEventListener('resize', this.updateVh.bind(this));
+  }
+  updateVh() {
+    this.root.style.setProperty('--vh', `${ this.vhValue()}px`);
+  }
+  vhValue() {
+    return window.innerHeight;
+  }
+};
+
+export default UpdateVh;
