d7055fb89a2d7d235e58b0bb5610e434fce8fe3b
yosri mhamdi <bavary1515@gmail.com>
2020-12-11 19:44:48 +0100

    improved build.js file.

--- app/gulp/tasks/build.js
+++ app/gulp/tasks/build.js
@@ -1,31 +1,34 @@
-const gulp  = require('gulp'),
-      clean = require('gulp-clean'),
-      imagemin = require('gulp-imagemin'),
-      gulpUsemin = require('gulp-usemin'),
-      cssnano = require('gulp-cssnano'),
-      uglify = require('gulp-uglify'),
-      rev = require('gulp-rev'),
-      scripts = require('./scripts'),
-      styles = require('./styles'),
-      server = require('./server');
+const gulp = require('gulp'),
+  clean = require('gulp-clean'),
+  imagemin = require('gulp-imagemin'),
+  gulpUsemin = require('gulp-usemin'),
+  cssnano = require('gulp-cssnano'),
+  uglify = require('gulp-uglify'),
+  rev = require('gulp-rev'),
+  scripts = require('./scripts'),
+  styles = require('./styles'),
+  server = require('./server');
 
-const deleteDist = () => {
-  return gulp.src('./dist', {read: false, allowEmpty: true})
-             .pipe(clean());
-};
-const compressImages = () => {
-  return gulp.src('./app/assets/images/**/*.*')
-             .pipe(imagemin())
-             .pipe(gulp.dest('./dist/assets/images/'));
-};
-const usemin = () => {
-  return gulp.src('./app/index.html')
-             .pipe(gulpUsemin({
-               css:[() => rev(), () => cssnano()],
-               js:[() => rev(), () => uglify()]
-             }))
-             .pipe(gulp.dest('./dist/'));
-};
+const deleteDist = () =>
+  gulp.src('./dist', { read: false, allowEmpty: true }).pipe(clean());
+
+const compressImages = () =>
+  gulp
+    .src('./app/assets/images/**/*.*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/assets/images/'));
+
+const usemin = () =>
+  gulp
+    .src('./app/index.html')
+    .pipe(
+      gulpUsemin({
+        css: [() => rev(), () => cssnano()],
+        js: [() => rev(), () => uglify()],
+      }),
+    )
+    .pipe(gulp.dest('./dist/'));
+
 const copyFolders = () => {
   const paths = [
     './app/**/*.*',
@@ -35,9 +38,21 @@ const copyFolders = () => {
     '!./app/temp/**/*.*',
     '!./app/gulp/',
     '!./app/gulp/**/*.*',
-    '!./app/index.html'
+    '!./app/index.html',
   ];
-  return gulp.src(paths)
-             .pipe(gulp.dest('./dist/'));
+
+  return gulp.src(paths).pipe(gulp.dest('./dist/'));
 };
-gulp.task('build', gulp.series(scripts.bundle, scripts.es5, styles.compileStyles ,deleteDist, compressImages, copyFolders, usemin, server.createServer.bind(this,'dist')));
+
+gulp.task(
+  'build',
+  gulp.series(
+    scripts.bundle,
+    scripts.es5,
+    styles.compileStyles,
+    deleteDist,
+    compressImages,
+    copyFolders,
+    usemin,
+  ),
+);
