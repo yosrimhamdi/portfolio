9238fcd08a8124729efb4a293c4f2264d3e080b8
yosri mhamdi <bavary1515@gmail.com>
2020-12-13 15:46:39 +0100

    removed scale animation on particules on button click.

--- app/assets/scripts/modules/SlideOnClick.js
+++ app/assets/scripts/modules/SlideOnClick.js
@@ -4,7 +4,6 @@ class SlideOnClick {
   constructor() {
     this.button = $('.button-main');
     this.contentWrapper = $('.content-wrapper');
-    this.particles = $('.particles');
     this.html = $('html');
 
     this.initialClasses();
@@ -17,12 +16,10 @@ class SlideOnClick {
 
   initialClasses() {
     this.contentWrapper.addClass('slide-on-click');
-    this.particles.addClass('scale-on-click');
   }
 
   animate() {
     this.contentWrapper.toggleClass('slide-on-click--clicked');
-    this.particles.toggleClass('scale-on-click--clicked');
   }
 }
 
