716e7c898b9a33726cd7ab739536a5ae4cdef4ab
yosri mhamdi <bavary1515@gmail.com>
2020-12-22 20:04:24 +0100

    extracted a service modifier to icon block.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -37,11 +37,7 @@ new CreateSkills({
 new AnimateOnWaypoint('.flipping-card', 'animate-f-cards-on-waypoint', '70%');
 new AnimateOnWaypoint('.news, .project', 'fade-in-on-waypoint', '70%');
 new AnimateOnWaypoint('.skill__percentage-bar', 'slide-on-waypoint', '96%');
-new AnimateOnWaypoint(
-  '.footer__github-icon',
-  'shake-on-waypoint',
-  'bottom-in-view',
-);
+new AnimateOnWaypoint('#github-icon', 'shake-on-waypoint', 'bottom-in-view');
 
 new FlipCardsOnHover();
 
